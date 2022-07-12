69e1c902ded1bdbfa985c4136287d78bcb293fc8
soliu yusuf <soliuy5@gmail.com>
2022-07-12 12:50:03 +0100

    remove warning

--- src/components/ListHeader.js
+++ src/components/ListHeader.js
@@ -1,6 +1,6 @@
-import { sortBy } from "lodash";
-import React, { useState } from "react";
-import Button from "./Button";
+
+import React,from "react";
+
 import Card from "./UI/Card";
 
 import classes from "./ListHeader.module.css";
